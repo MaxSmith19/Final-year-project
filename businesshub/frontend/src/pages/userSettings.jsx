f3fcc1974e1f8e43fb7ea35620837ba968f60eaf
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-03-16 17:07:53 +0000

    Logo implemented into page

--- businesshub/frontend/src/pages/userSettings.jsx
+++ businesshub/frontend/src/pages/userSettings.jsx
@@ -5,13 +5,26 @@ import * as qs from 'qs'
 
 const UserSettings = () => {
   const [businessName, setBusinessName] = useState('')
+  const [businessLogo, setBusinessLogo] = useState('')
   const navigate = useNavigate()
 
   const onSubmit = (e) => {
     e.preventDefault();
     var data = qs.stringify({
-      'businessName': businessName
+      'businessName': businessName,
+      'businessLogo': businessLogo
     });
+    data ={}
+    if(businessLogo === ''){
+      data.businessLogo = localStorage.getItem('businessLogo')
+    }else{
+      data.businessLogo = businessLogo
+    }
+    if(businessName === ''){
+      data.businessName = localStorage.getItem('businessName')
+    }else{
+      data.businessName = businessName
+    }
     var config = {
       method: 'put',
       maxBodyLength: Infinity,
@@ -43,7 +56,13 @@ const UserSettings = () => {
           <label className="block text-gray-700 text-xl font-bold mb-2">
             Name of business
           </label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3" type="text" onChange={(e) => setBusinessName(e.target.value)} value={businessName} required/>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3" type="text" onChange={(e) => setBusinessName(e.target.value)} value={businessName}/>
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-xl font-bold mb-2">
+            Business Logo
+          </label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3" type="file" onChange={(e) => setBusinessLogo(e.target.value)} value={businessLogo} />
         </div>
         <div className="flex items-center justify-between">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded " type="submit" >Save changes</button>
--- businesshub/mern/models/userModel.js
+++ businesshub/mern/models/userModel.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
     businessName: {type:String,required: true},
+    businessLogo: {type:String,required: false},
     email: {type:String,required:true},
-    password:{type:String,required:true}
+    password:{type:String,required:true},
 });
 
 module.exports = mongoose.model('User',userSchema)
