f44489976c02ca72b0308ac8cd3ba4121a404805
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-03-27 09:45:17 +0100

    Code comments added

--- businesshub/mern/controllers/userController.js
+++ businesshub/mern/controllers/userController.js
@@ -3,13 +3,16 @@ const bcrypt = require("bcrypt")
 const User = require("../models/UserModel");
 const { validEmail,validPassword } = require("../regex");
 const jwt = require("jsonwebtoken");
-const { RiCreativeCommonsZeroLine } = require("react-icons/ri");
+const {decodeJWT, generateToken} = require("../middleware/authMiddleware")
 
 //Returns all data on user based on their given mongo _id
 const getUser = asyncHandler(async (req, res) => {
     const token=decodeJWT(req,res)
+    //use the decode function from AuthMiddleware to decode the token
     const Users = await User.find({_id:token.id}).select("-password");
+    //recieve the users data using the token id, without their password being shown
     res.status(200).json(Users)
+    // OK - Sending the data back to the client
   })
 
 
@@ -17,18 +20,21 @@ const getUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async(req,res) =>{
     const {email} = req.body
     const user = await User.findOne({"email":email})
-
-
+    //findOne is needed as it find the first available user with the given email
+    //As there should only be one user with the given email
     if(!user){
         res.status(401).json("User not found")
         throw new Error("User not found")
+        //Throw an error using the errorMiddleware 
     }
 
     if(await(bcrypt.compare(req.body.password, user.password))){
+        //if the hashed password matches the hashed password in the database
         res.status(200).json({
             _id : user._id,
             token: generateToken(user._id)
         })
+        //send the token back to the client to be formed into a cookie
     }else{
         res.status(401).json("Password not correct")
         throw new Error("Wrong password")
@@ -39,7 +45,6 @@ const loginUser = asyncHandler(async(req,res) =>{
 const registerUser = asyncHandler(async(req, res) =>{
     const email = req.body.email
     const userExists = await User.findOne({email})
-    console.log(validEmail.test(req.body.email))
     if(userExists){
         res.status(401).json("User already exists")
         throw new Error("User already exists")
@@ -52,15 +57,18 @@ const registerUser = asyncHandler(async(req, res) =>{
     }
 
     const hashedPassword= await bcrypt.hash(req.body.password,10)
-        
+    //hash the password using the blowfish encryption algorithm, 
+    //"10", forms the salt that is added to the end of the password for additional security
     const users = await User.create({
         businessName:req.body.businessName,
         password: hashedPassword,
         email:req.body.email,
     })
+    //create a new user with the hashed password
     
     res.status(200).json({
         token: generateToken(users._id)
+        //send the token back to the client to be formed into a cookie
     })
 })
 
@@ -72,20 +80,9 @@ const updateUser = asyncHandler(async( req, res) =>{
         throw new Error("User not found")
     }
     const updatedUser = await User.findByIdAndUpdate(token.id,req.body,{new: true,})
+    //using the given data, update the user in the database with the content in the request body
     res.status(200).json(updatedUser)
 })
-
-const decodeJWT = (req, res) => {
-    const token = req.headers.authorization.split(" ")[1]
-    return decoded=jwt.verify(token, process.env.JWT_SECRET);
-}
-//for generating jwt tokens for authentication
-const generateToken = (id) => {
-
-    return jwt.sign({id}, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES,
-    })
-}
 
 const deleteUser = asyncHandler(async(req, res) =>{
     const token = decodeJWT(req,res)
--- businesshub/mern/routes/userRoutes.js
+++ businesshub/mern/routes/userRoutes.js
@@ -7,13 +7,16 @@ const {
     updateUser,
     deleteUser,
     } = require('../controllers/UserController')
-const { protect} = require("../middleware/authMiddleware")
+    //import all functions from the controllers file
 
+const { protect} = require("../middleware/authMiddleware")
+//Get the JWT protect function from the auth middleware file
 router.route('/').post(registerUser)
 router.get('/get', protect, getUser)
 router.route('/login/').post(loginUser);
 router.route('/update').put(protect,updateUser)
 router.route("/del").delete(protect,deleteUser)
-
+//Each of these show the associated subdirectory for the function.
+//So, if we want to register a user, we add a / at the end of the url 
 
 module.exports = router
