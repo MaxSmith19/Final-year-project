c4e43782939e0b0cd9ce3d2fb84b6a53e4003153
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-03-31 18:37:49 +0100

    directory changed for holding styles

--- businesshub/frontend/src/App.js
+++ businesshub/frontend/src/App.js
@@ -11,7 +11,7 @@ import Footer from "./Components/Footer";
 import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import "./animations.css";
+import "./styles/animations.css";
 import UserSettings from './pages/userSettings';
 
 function App() {
