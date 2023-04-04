b08f673b1031730d45fb91a2d593eb0d064aab11
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-04-04 14:20:23 +0100

    Inventory + Ingredients

--- businesshub/frontend/src/pages/inventory.jsx
+++ businesshub/frontend/src/pages/inventory.jsx
@@ -1,11 +1,12 @@
 import react, {useState, useEffect} from 'react';
 import axios from 'axios';
 import { FiSave } from 'react-icons/fi'
+import { toast } from 'react-toastify';
 const qs = require('qs');
 
 function Inventory() {
     const [inventoryRows, setInventoryRows] = useState([]);
-    const [ingredientsRows, setIngredientsRows] = useState([{}])
+    const [ingredientsRows, setIngredientsRows] = useState([])
     //These are the rows of the current inventory being shown
     const [cacheResponse, setCacheResponse] = useState([]);
     //Cache the entire response so that it doesn't have to be requested every time
@@ -54,14 +55,31 @@ function Inventory() {
         });
     }
 
-    const onChangeCell = (event, index, key) => {
+    const onChangeInventoryCell = (event, index, key) => {
         const newRows = [...inventoryRows];
         newRows[index][key] = event.target.value;
         setInventoryRows(newRows);
+        const button = document.getElementById("saveButton")
+        if(cacheResponse === ingredientsRows){
+            button.classList.add("hidden")
+        }else{
+            button.classList.remove("hidden")
+        }
     }
 
+    const onChangeIngredientsCell = (event, index, key) => {
+        const newRows = [...ingredientsRows];
+        newRows[index][key] = event.target.value;
+        setIngredientsRows(newRows);
+        const button = document.getElementById("saveButton")
+        if(cacheResponse===inventoryRows){
+            button.classList.add("hidden")
+            
+        }else{
+            button.classList.remove("hidden")
+        }
+    }
     const onSave = ()=> {
-        console.log(inventoryRows)
         const userIDCookie = document.cookie.split("=")[1];
         const token = userIDCookie.split(";")[0];
         
@@ -81,13 +99,14 @@ function Inventory() {
         
         axios.request(config)
         .then((response) => {
-                console.log(JSON.stringify(response.data));
+            toast.success("Inventory successfully updated")
+            setCacheResponse(response.data)
         })
         .catch((error) => {
-                console.log(error);
+            toast.error("An error occured:" +error.message)
         });
     }
-    const addRow = () =>{
+    const addInventoryRow = () =>{
         const newRow = {
             date: '',
             notes: '',
@@ -98,6 +117,21 @@ function Inventory() {
    
           setInventoryRows([...inventoryRows, newRow]);
           //adds a new blank row to the table
+
+    }
+    
+    const addIngredientsRow = () =>{
+        const newRow = {
+            date: '',
+            notes: '',
+            debit: '',
+            credit: '',
+            balance: ''
+          };
+   
+          setIngredientsRows([...ingredientsRows, newRow]);
+          //adds a new blank row to the table
+
     }
     const deleteRow = () =>{
         const updatedInventoryRows = [...inventoryRows];
@@ -106,8 +140,8 @@ function Inventory() {
     }
     return(
         <>
+        <h1 className='text-center text-6xl mb-10'> Stock Inventory</h1>
             <div className="bg-white pb-16 mb-10 w-full m-auto p-4 rounded-xl shadow-2xl">
-                <h1>Inventory</h1>
                 <table className="table-auto w-full h-auto md:table-fixed sm:table-fixed">
                 <thead>
                     <tr>
@@ -115,33 +149,38 @@ function Inventory() {
                         <th className="w-5/12 border-t border-l">Description</th>
                         <th className="w-1/12 border-t border-l">Quantity</th>
                         <th className="w-1/12 border-t border-l">Selling price</th>
-                        <button onClick={addRow}>+</button>
+                        <button onClick={addInventoryRow}>+</button>
 
                     </tr>
                 </thead>
                 <tbody >
                 {inventoryRows.map((row, index) => (
                 <tr key={index} className="h-10 sm:text-left" ref={event => (row[index] =event)}>
-                    <td className="border-2 ">
-                        <input value={row.Item} onChange={(event)=>onChangeCell(event, index, "Item")} type="date" className="w-full" required />    
+                    <td className="p-1">
+                        <label className="block w-full bg-slate-50 sm:hidden">Item name</label>
+                        <input className="pl-2 bg-slate-50 w-full shadow-sm" value={row.Item} onChange={(event)=>onChangeInventoryCell(event, index, "Item")} type="text" required />    
                     </td>
-                    <td className="border-2">
-                        <input value={row.Description} onChange={(event)=>onChangeCell(event, index, "Description")} type="text" className="w-full" required />
+                    <td className="p-1">
+                        <label className="block w-full bg-slate-50 sm:hidden">Description</label>
+                        <input className="pl-2 bg-slate-50 w-full shadow-sm" value={row.Description} onChange={(event)=>onChangeInventoryCell(event, index, "Description")} type="text" required />
                     </td>
-                    <td className="border-2">  
-                        <input value={row.Quantity} onChange={(event)=>onChangeCell(event, index, "Quantity")} type="number" className="w-full" required />
+                    <td className="p-1">  
+                        <label className="block w-full bg-slate-50 sm:hidden">Quantity</label>
+                        <input className="pl-2 bg-slate-50 w-full shadow-sm" value={row.Quantity} onChange={(event)=>onChangeInventoryCell(event, index, "Quantity")} type="number" required />
                     </td>
-                    <td className="border-2">
-                        <input value={row.SellingPrice} onChange={(event)=>onChangeCell(event, index, "SellingPrice")} type="number" className="w-full" required />
+                    <td className="p-1">
+                        <label className="block w-full bg-slate-50 sm:hidden">Selling Price</label>
+                        <input className="pl-2 bg-slate-50 w-full shadow-sm" value={row.SellingPrice} onChange={(event)=>onChangeInventoryCell(event, index, "SellingPrice")} type="number" required />
                     </td>
                 </tr>
                 ))}
                 </tbody>
                 </table>   
-            <button onClick={onSave} className="float-right rounded"><FiSave size={50}/></button>          
 
             </div>
-            {/* <div className="bg-white pb-16 w-full m-auto p-4 rounded-xl shadow-2xl">
+            <h1 className='text-center text-6xl mb-10'>Ingredients</h1>
+            <div className="bg-white pb-16 w-full m-auto p-4 rounded-xl shadow-2xl">
+
                 <table className="table-auto w-full h-auto md:table-fixed sm:table-fixed">
                 <thead>
                     <tr>
@@ -149,31 +188,33 @@ function Inventory() {
                         <th className="w-5/12 border-t border-l">Description</th>
                         <th className="w-1/12 border-t border-l">Quantity</th>
                         <th className="w-1/12 border-t border-l">Selling price</th>
-                        <button onClick={addRow}>+</button>
+                        <button onClick={addIngredientsRow}>+</button>
 
                     </tr>
                 </thead>
                 <tbody >
                 {ingredientsRows.map((row, index) => (
                 <tr key={index} className="h-10 sm:text-left" ref={event => (row[index] =event)}>
-                    <td className="border-2 ">
-                        <input value={row.Item} onChange={(event)=>onChangeCell(event, index, "date")} type="date" className="w-full" required />    
+                    <td className=" ">
+                        <input value={row.Item} onChange={(event)=>onChangeIngredientsCell(event, index, "date")} type="date" className="w-full" required />    
                     </td>
-                    <td className="border-2">
-                        <input value={row.Description} onChange={(event)=>onChangeCell(event, index, "Description")} type="text" className="w-full" required />
+                    <td className="">
+                        <input value={row.Description} onChange={(event)=>onChangeIngredientsCell(event, index, "Description")} type="text" className="w-full" required />
                     </td>
-                    <td className="border-2">  
-                        <input value={row.Quantity} onChange={(event)=>onChangeCell(event, index, "debit")} type="number" className="w-full" required />
+                    <td className="">  
+                        <input value={row.Quantity} onChange={(event)=>onChangeIngredientsCell(event, index, "debit")} type="number" className="w-full" required />
                     </td>
-                    <td className="border-2">
-                        <input value={row.SellingPrice} onChange={(event)=>onChangeCell(event, index, "credit")} type="number" className="w-full" required />
+                    <td className="">
+                        <input value={row.SellingPrice} onChange={(event)=>onChangeIngredientsCell(event, index, "credit")} type="number" className="w-full" required />
                     </td>
                 </tr>
                 ))}
                 </tbody>
                 </table>   
                 
-            </div> */}
+            </div>
+            <button onClick={onSave} id="saveButton"className="transition-all ease-in-out duration-75 rounded-full fixed bottom-10 right-10 bg-green-700 p-3 hidden"><FiSave size={50}/></button>          
+
             </>
         )
     }
