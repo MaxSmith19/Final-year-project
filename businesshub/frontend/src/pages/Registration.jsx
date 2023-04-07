92e64f4baab546739f53956febc74bb4d3a2f133
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-04-07 14:18:32 +0100

    registration converted to function

--- businesshub/frontend/src/pages/Registration.jsx
+++ businesshub/frontend/src/pages/Registration.jsx
@@ -1,68 +1,47 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import axios from "axios"
-import { Link, Navigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import * as qs from 'qs'
-import { useNavigate } from 'react-router-dom';
 import {toast} from 'react-toastify'
 
-export default class Registration extends Component {
-  constructor(props){
-    super(props);
+const Registration = () => {
+  const [businessName, setBusinessName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-    this.state = {
-      businessName: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-      errorMessage: ''
-    }
-
-    this.onChangeBusinessName = this.onChangeBusinessName.bind(this);
-    this.onChangeEmail = this.onChangeEmail.bind(this);
-    this.onChangePassword = this.onChangePassword.bind(this);
-    this.onChangeConfirmPassword = this.onChangeConfirmPassword.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    
-    const navigate = useNavigate.bind(this)
-  }
-  onChangeBusinessName(e){
-    this.setState({
-      businessName: e.target.value
-    })
-  }
-  onChangeEmail(e){
-    this.setState({ 
-      email: e.target.value
-    })
-  }
-  onChangePassword(e){
-    this.setState({
-      password: e.target.value
-    })
-  }
-  onChangeConfirmPassword(e){
-    this.setState({
-      confirmPassword: e.target.value
-    })
-
+  const onChangeBusinessName = (e) => {
+    setBusinessName(e.target.value);
   }
 
-  onSubmit(e){
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const onChangeConfirmPassword = (e) => {
+    setConfirmPassword(e.target.value);
+  }
+
+  const onSubmit = (e) => {
     e.preventDefault();
-    const dataArray = [this.state.email,this.state.businessName,this.state.password,this.state.confirmPassword]
-    let nullFlag = false
+    const dataArray = [email, businessName, password, confirmPassword];
+    let nullFlag = false;
     const data =  qs.stringify({
-      'email': this.state.email,
-      'businessName': this.state.businessName,
-      'password': this.state.password,
-      'confirmPassword': this.state.confirmPassword
-    })
+      'email': email,
+      'businessName': businessName,
+      'password': password,
+      'confirmPassword': confirmPassword
+    });
 
-    
     dataArray.forEach(function(item){
       if(item===""){
-        nullFlag=true
-        console.log("item is empty")
+        nullFlag=true;
+        console.log("item is empty");
       }
     });
 
@@ -81,6 +60,7 @@ export default class Registration extends Component {
         const token = response.data.token;
         document.cookie = "token=" + token +"; SameSite=Strict";
         axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
+        toast.success("Account successfully created")
       })
       .catch(function (error) {
         console.log(error.statusCode);
@@ -89,51 +69,51 @@ export default class Registration extends Component {
     }
   }
 
-  render() {
-    return(
-      <div class="flex justify-center">
+  return (
+    <div class="flex justify-center">
       <div class="max-w-lg mt-12 form-width form-length">
-      <h1 class="block text-gray-700 font-bold text-center text-5xl mb-10">Register an account</h1>
+        <h1 class="block text-gray-700 font-bold text-center text-5xl mb-10">Register an account</h1>
 
-      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={this.onSubmit}>
-        <div class="mb-4">
-          <label class="block text-gray-700 text-xl font-bold mb-2" for="businessName">
-            Name of business
-          </label>
-          <input class="shadow border rounded w-full py-2 px-3 text-gray-700"type="text" onChange={this.onChangeBusinessName} value={this.state.businessName}/>
-        </div>
-        <div class="mb-6">
-          <label class="block text-gray-700 text-xl font-bold mb-2" for="password">
-            Email address
-          </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3" type="text" onChange={this.onChangeEmail} value={this.state.email}/>
-        </div>
-        <div class="mb-6">
-          <label class="block text-gray-700 text-xl font-bold mb-2" for="password">
-            Password
-          </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700" type="password" onChange={this.onChangePassword} value={this.state.password}/>
-        </div>
-        <div class="mb-6">
-          <label class="block text-gray-700 text-xl font-bold mb-2" for="password">
-            Confirm password
-          </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3" type="password" onChange={this.onChangeConfirmPassword} value={this.state.confirmPassword}/>
-        </div>
-        <div>
-          <h1 className="mb-5">{this.state.errorMessage}</h1>
-        </div>
-        <div>
-          <h1 className="mb-5">Already have an account? Login <Link className="text-blue-600"to="/Login">here</Link></h1>
-        </div>
-        <div class="flex items-center justify-between">
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded " type="submit">
-            Submit
-          </button>
-        </div>
-      </form>
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={onSubmit}>
+          <div class="mb-4">
+            <label class="block text-gray-700 text-xl font-bold mb-2" for="businessName">
+              Name of business
+            </label>
+            <input class="shadow border rounded w-full py-2 px-3 text-gray-700" type="text" onChange={onChangeBusinessName} value={businessName}/>
+          </div>
+          <div class="mb-6">
+            <label class="block text-gray-700 text-xl font-bold mb-2" for="password">
+              Email address
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3" type="text" onChange={onChangeEmail} value={email}/>
+          </div>
+          <div class="mb-6">
+            <label class="block text-gray-700 text-xl font-bold mb-2" for="password">
+              Password
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700" type="password" onChange={onChangePassword} value={password}/>
+          </div>
+          <div class="mb-6">
+            <label class="block text-gray-700 text-xl font-bold mb-2" for="password">
+              Confirm password
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3" type="password" onChange={onChangeConfirmPassword} value={confirmPassword}/>
+          </div>
+          <div>
+            <h1 className="mb-5">{errorMessage}</h1>
+          </div>
+          <div>
+            <h1 className="mb-5">Already have an account? Login <Link className="text-blue-600"to="/Login">here</Link></h1>
+          </div>
+          <div class="flex items-center justify-between">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded " type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
-      </div>
+    </div>
+
     )
-  }
 }
+export default Registration
