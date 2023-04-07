fe03bd69244422bdf175e34c24492d2924c24b3c
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-04-07 13:59:56 +0100

    account details shown on dashboard

--- businesshub/frontend/src/pages/dashboard.jsx
+++ businesshub/frontend/src/pages/dashboard.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import qs from 'qs';
 import { useEffect } from 'react';
 import { RiAccountBoxFill } from 'react-icons/ri';
-
+import { MdEmail } from 'react-icons/md';
 const Dashboard = () =>{
     const [userInfo, setUserInfo] = useState(null);
     const [userEmail, setUserEmail] = useState("");
@@ -44,7 +44,6 @@ const Dashboard = () =>{
         .catch((error) => {
           console.log(error);
         });
-        
       }
       
     return(
@@ -52,17 +51,19 @@ const Dashboard = () =>{
                 <div className="bg-white h-full w-full m-auto rounded-xl shadow-2xl p-2">
                     <div>
                         {/* title */}
-                        <h1 className="text-5xl ">Your account</h1>
+                        <h1 className="text-5xl mb-3">Account details</h1>
                     </div>
                     <hr/>
                     <div>
                         {/* topHalf */}
-                        {/* todo image */}
+                        <img className="w-3/4 h-3/4 text-center m-auto mt-3 mb-3 shadow-md" src={`http://localhost:5000/${imageSrc}`} />
                     </div> 
-                    
+                    <hr />
                     <div className="w-full h-auto mt-4">
                         {/* bottomHalf */}
-                        <RiAccountBoxFill className='float-left' size="32"/><h2 className="text-2xl">Welcome {businessName}</h2>
+                        <RiAccountBoxFill className='float-left' size="32"/><h2 className="text-2xl">Welcome {businessName}!</h2>
+                        <MdEmail className="float-left" size="32"/><h2 className="text-2xl">{userEmail}</h2>
+                        <h2>Pre-alpha</h2>
                     </div>
                 </div>
                 <div className="bg-white h-96 w-full m-auto rounded-xl shadow-2xl p-2">
