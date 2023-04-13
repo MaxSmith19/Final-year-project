c5e76d20560c7c9f4505c37774e700e113bb035f
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-04-13 20:39:55 +0100

    removing unecessary files importing r-r dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
 import './styles/index.css';
 import './styles/mediaQueries.css'
@@ -9,9 +7,7 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-    <Provider store={store}>
       <App />
-    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
