05ab6cffe92c67953805d2a658f0880b2492958d
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-04-13 20:02:38 +0100

    checking uri

--- mern/config/db.js
+++ mern/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URI);
+    const connect = await mongoose.connect("mongodb+srv://root:root@cluster0.w3jh7u7.mongodb.net/BHUB");
     console.log(`CONNECTED TO MONGO @ ${connect.connection.host}`.underline);
   } catch (error) {
     console.log(error);
