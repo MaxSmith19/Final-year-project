c55a0b4575b730d621703eec7933f2da8d1d6804
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-04-14 13:32:21 +0100

    cors

--- mern/server.js
+++ mern/server.js
@@ -21,7 +21,7 @@ app.use('/uploads', express.static('uploads'));
 
 // Add the following middleware to include the Access-Control-Allow-Origin header
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', process.env.APP_URL);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
     next();
