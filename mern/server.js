eb6605cb4c5631e17d79db1d8785bab171d39880
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-04-15 11:30:10 +0100

    Mongo Logging added

--- mern/server.js
+++ mern/server.js
@@ -12,7 +12,12 @@ const cookieParser = require("cookie-parser")
 connectDB()
 
 const app=express()
-
+app.use((req, res, next) => {
+    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    //get the clients IP connection
+    next();
+  });
+  
 app.use(express.json())
 app.use(express.urlencoded(false))
 app.use(cors())
