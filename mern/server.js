be56f936fdbc9238696d671e385cbdf5d43c2d59
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-04-16 11:57:07 +0100

    string error in server

--- mern/server.js
+++ mern/server.js
@@ -15,7 +15,10 @@ const app=express()
 
 app.use(express.json())
 app.use(express.urlencoded(false))
-app.use(cors({ origin: 'http://82.20.49.101', "http://localhost:3000" }));
+app.use(cors({ 
+  origin: ['http://82.20.49.101', 'http://localhost:3000'], 
+  credentials: true 
+}));
 app.use((req, res, next) => {
   console.log(req.headers.origin)
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
