654c879eab55183f4be02060f6138f95da37e576
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-04-16 11:27:17 +0100

    checking cors

--- mern/server.js
+++ mern/server.js
@@ -15,17 +15,10 @@ const app=express()
 
 app.use(express.json())
 app.use(express.urlencoded(false))
-app.use(cors({origin: process.env.APP_URL}))
+app.use(cors({ origin: 'http://82.20.49.101' }));
+
 app.use(cookieParser())
 app.use('/uploads', express.static('uploads'));
-
-// Add the following middleware to include the Access-Control-Allow-Origin header
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', process.env.APP_URL);
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-    next();
-  });
   
 
 app.use('/api/Users', require("./routes/userRoutes.js"))
