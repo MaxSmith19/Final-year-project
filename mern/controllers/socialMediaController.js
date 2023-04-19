87cd8eb7ae0ab639a80c8145fae0cfca655f3af1
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-04-19 15:28:33 +0100

    Etsy to work on server

--- mern/controllers/socialMediaController.js
+++ mern/controllers/socialMediaController.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const {decodeJWT, generateToken} = require("../middleware/authMiddleware")
 const Social = require("../models/socialMediaModel")
 const crypto = require("crypto")
-// const axios = require("axios")
+const axios = require("axios")
 
 const etsyRedirectUrl= "http://localhost:5000/api/Socials/etsyCallback"
 const base64URLEncode = (str) =>
